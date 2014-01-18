9b12b3c34243e68ca53b090c364db91e21db25c9
Robin Bonatesta <rbonates@kent.edu>
2014-01-18 04:17:26 -0500

    login and register

--- gen/com/pdhurbnl/somato/R.java
+++ gen/com/pdhurbnl/somato/R.java
@@ -21,10 +21,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int somato=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int PasswordField=0x7f080002;
+        public static final int UserField=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int background=0x7f080000;
+        public static final int enter=0x7f080004;
+        public static final int somatoText=0x7f080003;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -37,6 +44,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int login=0x7f050004;
+        public static final int submit=0x7f050005;
+        public static final int user_hint=0x7f050003;
     }
     public static final class style {
         /** 
